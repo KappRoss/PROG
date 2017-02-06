8c8f8caef78a87e3e848c88611396d5e38c919f1
KappRoss <dikun_1996@mail.ru>
2017-02-06 11:03:41 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main() 
+{
+	// 1. P = 4 * a:
+	float a;
+	cout << "Enter the length of the square: ";
+	cin >> a;
+	float p;
+	p = a;
+	p *= 4;
+	cout << "The perimeter of the square = " << p << endl;
+	// 2. S = a^2
+	float s;
+	s = a;
+	s *= s;
+	cout << "Square area = " << s << endl;
+
+	system("pause");
+
+
+}
